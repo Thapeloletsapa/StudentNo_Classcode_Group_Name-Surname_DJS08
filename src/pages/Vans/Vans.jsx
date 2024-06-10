c5be686ce414e710f8ea1cc9758714a23b1a2f84
Thapelo <thapeloletsapa@gmail.com>
2024-06-10 15:27:46 +0200

    "Refactor Vans component to use useSearchParams and getVans API, add loading and error states"

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,33 +1,40 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link, useSearchParams } from "react-router-dom"
+import { getVans } from "../../api"
+
 
 export default function Vans() {
+    const [searchParams, setSearchParams] = useSearchParams()
     const [vans, setVans] = React.useState([])
+    const [loading, setLoading] = React.useState(false)
+    const [error, setError] = React.useState(null)
+
+    const typeFilter = searchParams.get("type")
+
     React.useEffect(() => {
-        fetch("/api/vans")
-            .then(res => res.json())
-            .then(data => setVans(data.vans))
-    }, [])
+        async function loadVans() {
+            setLoading(true)
+            try {
+                const data = await getVans()
+                setVans(data)
+            } catch (err) {
+                setError(err)
+            } finally {
+                setLoading(false)
+            }
+        }
+        const typeFilter = searchParams.get("type")
 
-    const vanElements = vans.map(van => (
-        <div key={van.id} className="van-tile">
-            <Link to={`/vans/${van.id}`}>
-                <img src={van.imageUrl} />
-                <div className="van-info">
-                    <h3>{van.name}</h3>
-                    <p>${van.price}<span>/day</span></p>
-                </div>
-                <i className={`van-type ${van.type} selected`}>{van.type}</i>
-            </Link>
-        </div>
-    ))
-
-    return (
-        <div className="van-list-container">
-            <h1>Explore our van options</h1>
-            <div className="van-list">
-                {vanElements}
-            </div>
-        </div>
-    )
-}
+        React.useEffect(() => {
+            async function loadVans() {
+                setLoading(true)
+                try {
+                    const data = await getVans()
+                    setVans(data)
+                } catch (err) {
+                    setError(err)
+                } finally {
+                    setLoading(false)
+                }
+            }
+    
