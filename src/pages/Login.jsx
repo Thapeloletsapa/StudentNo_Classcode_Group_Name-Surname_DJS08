1afecfa320e83d24a8175e5e286f332a26365626
Thapelo <thapeloletsapa@gmail.com>
2024-06-10 15:31:55 +0200

    "Added state variables and function declaration to Login component"

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,10 @@
 import React from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 import { loginUser } from "../api"
+
+
+
+export default function Login() {
+    const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
+    const [status, setStatus] = React.useState("idle")
+    const [error, setError] = React.useState(null)
