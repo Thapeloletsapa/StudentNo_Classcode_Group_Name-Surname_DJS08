c6b66f1d9e3e222860e297d81ba4e3e989fd5189
Thapelo <thapeloletsapa@gmail.com>
2024-06-10 15:32:40 +0200

    "Added error message display to Login component"

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -39,3 +39,15 @@ export default function Login() {
             [name]: value
         }))
     }
+
+    return (
+        <div className="login-container">
+            {
+                location.state?.message &&
+                    <h3 className="login-error">{location.state.message}</h3>
+            }
+            <h1>Sign in to your account</h1>
+            {
+                error?.message &&
+                    <h3 className="login-error">{error.message}</h3>
+            }
