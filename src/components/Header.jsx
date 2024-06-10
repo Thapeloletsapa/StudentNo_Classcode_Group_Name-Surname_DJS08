708d6a26012100d869947356ebde7db5df32b158
Thapelo <thapeloletsapa@gmail.com>
2024-06-10 15:58:36 +0200

    "Added activeStyles object and fakeLogOut function to Header component"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,24 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 export default function Header() {
+
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+    
+    function fakeLogOut() {
+        localStorage.removeItem("loggedin")
+    }
+
+
+
+
+
+
+
+
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
