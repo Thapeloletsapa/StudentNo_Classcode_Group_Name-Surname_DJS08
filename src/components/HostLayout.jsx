2765f49159ea08551c85ce75a7829f34fa694744
Thapelo <thapeloletsapa@gmail.com>
2024-06-10 16:03:00 +0200

    "Added import statement and default function HostLayout with activeStyles object in HostLayout.jsx"

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,1 +1,9 @@
 import { NavLink, Outlet } from "react-router-dom"
+
+
+export default function HostLayout() {
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
