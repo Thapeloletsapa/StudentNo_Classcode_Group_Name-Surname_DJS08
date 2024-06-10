8f38d2ff84c622ebbe94e666b63734d178192223
Thapelo <thapeloletsapa@gmail.com>
2024-06-10 15:47:47 +0200

    "Added routes to server: GET /vans, GET /vans/:id, GET /host/vans, GET /host/vans/:id, and POST /login"

--- src/server.jsx
+++ src/server.jsx
@@ -16,3 +16,41 @@ createServer({
 
         server.create("user", { id: "123", email: "b@b.com", password: "p123", name: "Bob" });
     },
+    routes() {
+        this.namespace = "api";
+        this.logging = false;
+
+        this.get("/vans", (schema) => {
+            return schema.vans.all();
+        });
+
+        this.get("/vans/:id", (schema, request) => {
+            const id = request.params.id;
+            return schema.vans.find(id);
+        });
+
+        this.get("/host/vans", (schema) => {
+            return schema.vans.where({ hostId: "123" });
+        });
+
+        this.get("/host/vans/:id", (schema, request) => {
+            const id = request.params.id;
+            return schema.vans.findBy({ id, hostId: "123" });
+        });
+
+        this.post("/login", (schema, request) => {
+            const { email, password } = JSON.parse(request.requestBody);
+            const foundUser = schema.users.findBy({ email, password });
+
+            if (!foundUser) {
+                return new Response(401, {}, { message: "No user with those credentials found!" });
+            }
+
+            foundUser.password = undefined;
+            return {
+                user: foundUser,
+                token: "Enjoy your pizza, here's your tokens."
+            };
+        });
+    }
+});
