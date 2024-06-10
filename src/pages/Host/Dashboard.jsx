f8ec9c25deff1676a4f2c78a3d1974ae90d4df66
Thapelo <thapeloletsapa@gmail.com>
2024-06-10 14:09:15 +0200

    "Added loading and error handling, and refactored Dashboard component to include new sections for earnings, reviews, and vans"

--- src/pages/Host/Dashboard.jsx
+++ src/pages/Host/Dashboard.jsx
@@ -33,3 +33,52 @@ export default function Dashboard() {
             </div>
         )
     }
+    if (loading) {
+         return <h1>Loading...</h1>
+     }
+
+    if (error) {
+        return <h1>Error: {error.message}</h1>
+    }
+
+    return (
+        <>
+            <section className="host-dashboard-earnings">
+                <div className="info">
+                    <h1>Welcome!</h1>
+                    <p>Income last <span>30 days</span></p>
+                    <h2>$2,260</h2>
+                </div>
+                <Link to="income">Details</Link>
+            </section>
+            <section className="host-dashboard-reviews">
+                <h2>Review score</h2>
+
+                <BsStarFill className="star" />
+
+                <p>
+                    <span>5.0</span>/5
+                </p>
+                <Link to="reviews">Details</Link>
+            </section>
+            <section className="host-dashboard-vans">
+                <div className="top">
+                    <h2>Your listed vans</h2>
+                    <Link to="vans">View all</Link>
+                </div>
+                {
+                    loading && !vans
+                    ? <h1>Loading...</h1>
+                    : (
+                        <>
+                            {renderVanElements(vans)}
+                        </>
+                    )
+                }
+                <React.Suspense fallback={<h3>Loading...</h3>}>
+                    <Await resolve={loaderData.vans}>{renderVanElements}</Await>
+                </React.Suspense>
+            </section>
+        </>
+    )
+}
