fa0ed0b897ece5fb33c7556ba80a2661f2a94d07
Thapelo <thapeloletsapa@gmail.com>
2024-06-10 15:33:00 +0200

    "Added form with email and password inputs, and a submit button to Login component"

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -51,3 +51,30 @@ export default function Login() {
                 error?.message &&
                     <h3 className="login-error">{error.message}</h3>
             }
+
+<form onSubmit={handleSubmit} className="login-form">
+                <input
+                    name="email"
+                    onChange={handleChange}
+                    type="email"
+                    placeholder="Email address"
+                    value={loginFormData.email}
+                />
+                <input
+                    name="password"
+                    onChange={handleChange}
+                    type="password"
+                    placeholder="Password"
+                    value={loginFormData.password}
+                />
+                <button
+                    disabled={status === "submitting"}
+                >
+                    {status === "submitting"
+                        ? "Logging in..."
+                        : "Log in"
+                    }
+                </button>
+            </form>
+        </div>
+    )
