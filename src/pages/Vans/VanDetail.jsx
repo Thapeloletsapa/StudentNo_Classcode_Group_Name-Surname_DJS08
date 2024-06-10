728088c45bf9ef6fe59d985dd0bd51aebe404565
Thapelo <thapeloletsapa@gmail.com>
2024-06-10 14:33:51 +0200

    "Added useLocation and getVans import to VanDetail component"

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -1,5 +1,6 @@
 import React from "react"
-import { useParams } from "react-router-dom"
+import { Link, useParams, useLocation } from "react-router-dom"
+import { getVans } from "../../api"
 
 export default function VanDetail() {
     const params = useParams()
