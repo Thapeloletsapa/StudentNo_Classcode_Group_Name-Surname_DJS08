47c64d4636217655792c689748dea33445ca7e99
Thapelo <thapeloletsapa@gmail.com>
2024-06-10 14:34:56 +0200

    "Refactor VanDetail component to use async/await and try-catch block for fetching van data"

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -11,11 +11,22 @@ export default function VanDetail() {
     const location = useLocation()
 
     React.useEffect(() => {
-        fetch(`/api/vans/${params.id}`)
-            .then(res => res.json())
-            .then(data => setVan(data.vans))
-    }, [params.id])
+        async function loadVans() {
+            setLoading(true)
+            try {
+                const data = await getVans(id)
+                setVan(data)
+            } catch (err) {
+                setError(err)
+            } finally {
+                setLoading(false)
+            }
+        }
+        loadVans()
+    }, [id])
 
+
+    
     return (
         <div className="van-detail-container">
             {van ? (
