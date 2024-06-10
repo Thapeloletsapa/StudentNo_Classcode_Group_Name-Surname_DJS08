6a27550882b84fae40460d04a95578da3d8c4f5a
Thapelo <thapeloletsapa@gmail.com>
2024-06-10 16:01:07 +0200

    update header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,21 +12,40 @@ export default function Header() {
     function fakeLogOut() {
         localStorage.removeItem("loggedin")
     }
-
-
-
-
-
-
-
-
+    
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
             <nav>
-                <Link to="/about">About</Link>
-                <Link to="/vans">Vans</Link>
+                <NavLink
+                    to="/host"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Host
+                </NavLink>
+                <NavLink
+                    to="/about"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    About
+                </NavLink>
+                <NavLink
+                    to="/vans"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Vans
+                </NavLink>
+                <Link to="login" className="login-link">
+                    <img
+                        src="src/assets/images/avatar-icon.png"
+                        className="login-icon"
+                    />
+                </Link>
+                <button onClick={fakeLogOut}>X</button>
             </nav>
         </header>
     )
 }
+
+
+
